277ed208f37b92c34ea330f3a3a2bde5652f23fc
Kolin Krewinkel <kolin.krewinkel@me.com>
2011-10-19 16:16:17 -0700

    Initial Commit

---
+++ KKNetworking/KKNetworking.h
@@ -0,0 +1,13 @@
+//
+//  KKNetworking.h
+//  KKNetworking
+//
+//  Created by Kolin Krewinkel on 10.19.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KKNetworking : NSObject
+
+@end
---
+++ KKNetworking/KKNetworking.m
@@ -0,0 +1,23 @@
+//
+//  KKNetworking.m
+//  KKNetworking
+//
+//  Created by Kolin Krewinkel on 10.19.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "KKNetworking.h"
+
+@implementation KKNetworking
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
